91e448077e45f5d773711a6799b1c8a55fc01ced
Erik Wessman <erikwessman10@gmail.com>
2021-12-03 17:50:15 +0100

    Cleaned up day3_part2

--- day3/day3_part2.py
+++ day3/day3_part2.py
@@ -1,54 +1,33 @@
 file = open("day3/input.txt", "r")
 
-array = file.read().splitlines()
-
-array_copy = array.copy()
+o2_array = file.read().splitlines()
+co2_array = o2_array.copy()
 
 o2 = ''
 co2 = ''
 
-for i in range(12):
-    most_common = 0
-    for line in array:
-        bit = int(line[i])
-        if bit == 1:
-            most_common += 1
-        else:
-            most_common -= 1
-    
-    new_list = []
-    if most_common >= 0:
-        new_list = [s for s in array if s[i] == '1']
-    elif most_common < 0:
-        new_list = [s for s in array if s[i] == '0']
+def solve(array, a, b):
+    for i in range(12):
+        most_common = 0
+        for line in array:
+            bit = int(line[i])
+            if bit == 1:
+                most_common += 1
+            else:
+                most_common -= 1
+        
+        new_list = []
+        if most_common >= 0:
+            new_list = [s for s in array if s[i] == a]
+        elif most_common < 0:
+            new_list = [s for s in array if s[i] == b]
 
-    array = new_list
+        array = new_list
 
-    if len(array) == 1:
-        o2 = array[0]
-        break
+        if len(array) == 1:
+            return array[0]
 
-array = array_copy
+o2 = solve(o2_array, '1', '0')
+co2 = solve(co2_array, '0', '1')
 
-for i in range(12):
-    most_common = 0
-    for line in array:
-        bit = int(line[i])
-        if bit == 1:
-            most_common += 1
-        else:
-            most_common -= 1
-    
-    new_list = []
-    if most_common >= 0:
-        new_list = [s for s in array if s[i] == '0']
-    elif most_common < 0:
-        new_list = [s for s in array if s[i] == '1']
-
-    array = new_list
-
-    if len(array) == 1:
-        co2 = array[0]
-        break
-    
 print(int(o2, 2) * int(co2, 2))
